afb7bd4b943fe2d9b4aea012ec699c1364fd49e4
Nicole Unknown <nicole.pah@googlemail.com>
2017-11-26 00:27:41 +0100

    Logo changed, too tired

--- indeks.js
+++ indeks.js
@@ -1,14 +1,22 @@
 const express = require('express');
 
 const app = express();
+const bodyparser = require('body-parser');
+
+const exps = ["java","mongodb","git","svg","mysql","docker","javascript"];
 
 // set view
 app.set('view engine', 'ejs');
+// set bodyparser
+app.use(bodyparser.urlencoded({ extended: true }));
+// public directory
 app.use(express.static('./assets'));
 
 // link 2 index page 
-app.get('/', function(req, res) {
-    res.render('pages/indeks');
+app.get('/', function(req, res){
+    res.render('pages/indeks',{
+        exps
+    });
 });
 
 app.listen(8080, (err) => {
